5ecfce55a3f28f221ef6d19becfa407a10130d69
Benny Wang <Krixium@users.noreply.github.com>
2018-02-01 13:19:12 -0800

    Added hotel rerouting (#16)

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['bananas/rule'] = function() {
+    return 'golf';
+};
 $route['i/.*'] = 'golf';
 $route['foxtrot'] = 'tango';
